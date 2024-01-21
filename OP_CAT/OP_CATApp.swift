5c81d0a9063d521fa78ffdf6c0c446dc2ae43ab5
Oleh Haidar <oleh.haidar@gmail.com>
2024-01-21 15:54:38 +0200

    Initial Commit

---
+++ OP_CAT/OP_CATApp.swift
@@ -0,0 +1,17 @@
+//
+//  OP_CATApp.swift
+//  OP_CAT
+//
+//  Created by Oleh Haidar on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct OP_CATApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
